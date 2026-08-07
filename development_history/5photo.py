cc8da2dfd111d72af1f3d531af198aae36c8d45b
rub9126 <rub9126@gmail.com>
2026-08-07 15:41:51 +0900

    Update 5photo.py

--- development_history/5photo.py
+++ development_history/5photo.py
@@ -49,7 +49,7 @@ def home():
 
     # 検索フォームのHTML
     html = f"""
-        <h1>喫茶店マップサイト準備中</h1>
+        <h1>喫茶店検索サイト準備中</h1>
         <form method="get">
             <label><input type="radio" name="smoking" value="nonsmoking_only" onclick="toggleRadio(this)" {"checked" if smoking == "nonsmoking_only" else ""}> 禁煙のみ</label>
             <label><input type="radio" name="smoking" value="nonsmoking_and_separated" onclick="toggleRadio(this)" {"checked" if smoking == "nonsmoking_and_separated" else ""}> 禁煙+分煙(禁煙席あり)</label>
